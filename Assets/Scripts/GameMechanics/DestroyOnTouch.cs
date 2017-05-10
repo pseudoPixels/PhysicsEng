026329bfbf646948de1a40a0394726ce87fc35ea
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2017-05-10 04:28:03 -0600

    Added the basic project to git

---
+++ Assets/Scripts/GameMechanics/DestroyOnTouch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100f);
+            Debug.Log(hit.collider.name);
+            if (hit.collider != null)
+            {
+                if(hit.collider.tag == "DestroyableBox")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameMechanics/PlayerTilt.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTilt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, GetComponent<Rigidbody2D>().velocity.y, 0.0f);
+        //rigidbody2D.velocity = movement * 10.0f;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(moveHorizontal, 0));
+
+        Debug.Log("Tilt");
+    }
+}
